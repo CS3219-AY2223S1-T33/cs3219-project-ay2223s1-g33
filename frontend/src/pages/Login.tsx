53ee36c1ce600ad816fb716835d158752535e829
kwanhw <kwanhaowei@gmail.com>
2022-08-31 23:31:01 +0800

    feat: add form validation logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,38 @@
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Stack, Text } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Heading,
+	Input,
+	Stack,
+	Text,
+} from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import Link from "../components/ui/Link";
 
 function Login() {
+	const {
+		register,
+		handleSubmit,
+		// watch,
+		formState: { errors },
+	} = useForm();
+
+	const validFormHandler = (data: any) => {
+		// ! At this point, submit to the backend to login
+		console.info("valid");
+		console.log(data);
+	};
+
+	const invalidFormHandler = (data: any) => {
+		console.warn("invalid");
+		console.log(data);
+	};
+
 	return (
 		<Flex minH="100vh" align="center" justify="center" bg="gray.50">
 			<Stack spacing={8} mx="auto" py={12} px={6} minW="45vw" maxW="65vw">
@@ -11,22 +41,36 @@ function Login() {
 				</Stack>
 
 				<Box rounded="lg" bg="white" boxShadow="lg" p={8}>
-					<Stack spacing={6}>
-						<FormControl id="email">
-							<FormLabel>Email address</FormLabel>
-							<Input type="email" />
-						</FormControl>
-						<FormControl id="password">
-							<FormLabel>Password</FormLabel>
-							<Input type="password" />
-						</FormControl>
-						{/* Forgot password and register */}
-						<Button>Sign in</Button>
+					<form onSubmit={handleSubmit(validFormHandler, invalidFormHandler)}>
+						<Stack spacing={6}>
+							<FormControl id="email" isInvalid={!!errors.email}>
+								<FormLabel>Email address</FormLabel>
+								<FormErrorMessage>{errors.email?.message as string}</FormErrorMessage>
+								<Input type="text" {...register("email", { required: "Please enter your email." })} />
+							</FormControl>
 
-						<Text align="center">
-							Not a user? <Link to="/register">Register</Link>
-						</Text>
-					</Stack>
+							<FormControl id="password" isInvalid={!!errors.password}>
+								<FormLabel>Password</FormLabel>
+								<Input
+									type="password"
+									{...register("password", {
+										required: "Please enter your password",
+										minLength: {
+											value: 8,
+											message: "Please make sure your password is at least 8 characters long.",
+										},
+									})}
+								/>
+								<FormErrorMessage>{errors.password?.message as string}</FormErrorMessage>
+							</FormControl>
+
+							<Button type="submit">Sign in</Button>
+
+							<Text align="center">
+								Not a user? <Link to="/register">Register</Link>
+							</Text>
+						</Stack>
+					</form>
 				</Box>
 			</Stack>
 		</Flex>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -12,63 +12,97 @@ import {
 	Button,
 	useBoolean,
 	Text,
+	FormErrorMessage,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import React from "react";
+import { useForm } from "react-hook-form";
 import Link from "../components/ui/Link";
 
 function Register() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 	const [showPassword, setShowPassword] = useBoolean();
+
+	const validFormHandler = (data: any) => {
+		// ! At this point, submit to the backend to login
+		console.info("valid");
+		console.log(data);
+	};
+
+	const invalidFormHandler = (data: any) => {
+		console.warn("invalid");
+		console.log(data);
+	};
 
 	return (
 		<Flex minH="100vh" align="center" justify="center" bg="gray.50">
 			<Stack spacing={8} mx="auto" py={12} px={6} minW="45vw" maxW="65vw">
 				<Stack align="center">
 					<Heading fontSize="4xl">Sign up</Heading>
-					{/* <Text fontSize="lg" color="gray.600">
-						to enjoy all of our cool features ✌️
-					</Text> */}
 				</Stack>
 				<Box rounded="lg" bg={useColorModeValue("white", "gray.700")} boxShadow="lg" p={8}>
-					<Stack spacing={4}>
-						<FormControl id="nickname" isRequired>
-							<FormLabel>Nickname</FormLabel>
-							<Input type="text" />
-						</FormControl>
-						<FormControl id="email" isRequired>
-							<FormLabel>Email address</FormLabel>
-							<Input type="email" />
-						</FormControl>
-						<FormControl id="password" isRequired>
-							<FormLabel>Password</FormLabel>
-							<InputGroup>
-								<Input type={showPassword ? "text" : "password"} />
-								<InputRightElement h="full">
-									<Button variant="ghost" onClick={setShowPassword.toggle}>
-										{showPassword ? <ViewIcon /> : <ViewOffIcon />}
-									</Button>
-								</InputRightElement>
-							</InputGroup>
-						</FormControl>
-						<Stack spacing={10} pt={2}>
-							<Button
-								loadingText="Submitting"
-								size="lg"
-								bg="blue.400"
-								color="white"
-								_hover={{
-									bg: "blue.500",
-								}}
-							>
-								Sign up
-							</Button>
+					<form onSubmit={handleSubmit(validFormHandler, invalidFormHandler)}>
+						<Stack spacing={4}>
+							<FormControl id="nickname" isInvalid={!!errors.nickname}>
+								<FormLabel>Nickname</FormLabel>
+								<Input
+									type="text"
+									{...register("nickname", { required: "Please enter your nickname." })}
+								/>
+								<FormErrorMessage>{errors.nickname?.message as string}</FormErrorMessage>
+							</FormControl>
+							<FormControl id="email" isInvalid={!!errors.email}>
+								<FormLabel>Email address</FormLabel>
+								<Input type="email" {...register("email", { required: "Please enter your email." })} />
+								<FormErrorMessage>{errors.email?.message as string}</FormErrorMessage>
+							</FormControl>
+							<FormControl id="password" isInvalid={!!errors.password}>
+								<FormLabel>Password</FormLabel>
+								<InputGroup>
+									<Input
+										type={showPassword ? "text" : "password"}
+										{...register("password", {
+											required: "Please enter your password",
+											minLength: {
+												value: 8,
+												message:
+													"Please make sure your password is at least 8 characters long.",
+											},
+										})}
+									/>
+									<InputRightElement h="full">
+										<Button variant="ghost" onClick={setShowPassword.toggle}>
+											{showPassword ? <ViewIcon /> : <ViewOffIcon />}
+										</Button>
+									</InputRightElement>
+								</InputGroup>
+								<FormErrorMessage>{errors.password?.message as string}</FormErrorMessage>
+							</FormControl>
+							<Stack spacing={10} pt={2}>
+								<Button
+									loadingText="Submitting"
+									size="lg"
+									bg="blue.400"
+									color="white"
+									_hover={{
+										bg: "blue.500",
+									}}
+									type="submit"
+								>
+									Sign up
+								</Button>
+							</Stack>
+							<Stack pt={6}>
+								<Text align="center">
+									Already a user? <Link to="/">Login</Link>
+								</Text>
+							</Stack>
 						</Stack>
-						<Stack pt={6}>
-							<Text align="center">
-								Already a user? <Link to="/">Login</Link>
-							</Text>
-						</Stack>
-					</Stack>
+					</form>
 				</Box>
 			</Stack>
 		</Flex>
