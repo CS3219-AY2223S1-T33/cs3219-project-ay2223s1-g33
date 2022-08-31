0d5bf5ac7ab38d859ab2cc59153f768727cdd205
sharpstorm <chimchar17@gmail.com>
2022-08-31 22:05:17 +0800

    chore: improve CI workflow

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -7,8 +7,16 @@ for P in ${DIRECTORIES[@]}; do
     CURRENT=`pwd`
     echo "Entering folder: ${CURRENT}"
     npm run-script lint
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+
     npm test
-    
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+
+    npm run-script build
     if [ $? -ne 0 ]; then
         exit 1
     fi
