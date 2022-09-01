0d39ae9681523d7fce5a40b1ba9ed54069234a6c
sharpstorm <chimchar17@gmail.com>
2022-09-02 01:02:37 +0800

    feat: add JWT token for login

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 
 import getApiServer from './api_server/api_server';
+import createAuthenticationService from './auth/authentication_service';
+import { IAuthenticationService } from './auth/authentication_service_types';
 import UserBFFServiceApi from './controller/user_service_bff_controller';
 import UserServiceApi from './controller/user_service_controller';
 import AppStorage from './storage/app_storage';
@@ -9,6 +11,9 @@ import loadEnvironment from './utils/env_loader';
 const envConfig = loadEnvironment();
 
 const dataStore: AppStorage = new AppStorage();
+const authService: IAuthenticationService = createAuthenticationService(
+  envConfig.JWT_SIGNING_SECRET,
+);
 
 const apiServer = getApiServer(envConfig.HTTP_PORT, envConfig.GRPC_PORT);
 const expressApp = apiServer.getHttpServer();
@@ -18,5 +23,5 @@ expressApp.get('/', (_: Request, resp: Response) => {
 });
 
 apiServer.registerServiceRoutes(new UserServiceApi(dataStore));
-apiServer.registerServiceRoutes(new UserBFFServiceApi());
+apiServer.registerServiceRoutes(new UserBFFServiceApi(authService));
 apiServer.bind();
