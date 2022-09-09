3b8765238d2bcc5554a0092a4ce2d73001a663af
Zheng Teck <adamgoh1998@gmail.com>
2022-09-09 17:10:27 +0800

    chore: rebuild matching-service with typescript template

---
+++ matching-service/src/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import getApiServer from './api_server/api_server';
+import loadEnvironment from './utils/env_loader';
+
+const envConfig = loadEnvironment();
+
+const apiServer = getApiServer(envConfig.HTTP_PORT, envConfig.GRPC_PORT);
+const expressApp = apiServer.getHttpServer();
+
+expressApp.get('/', (_: Request, resp: Response) => {
+    resp.status(200).send('Welcome to Matching Service');
+  });
+
+apiServer.bind();
