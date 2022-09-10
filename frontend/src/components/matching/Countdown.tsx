9ec55c2f92a3653e86c19fa097615d715e1136da
KwanHW <kwanhaowei@gmail.com>
2022-09-10 18:10:14 +0800

    fix: handle premature redirect before component is mounted for Countdown

--- frontend/src/components/matching/Countdown.tsx
+++ frontend/src/components/matching/Countdown.tsx
@@ -8,54 +8,59 @@ import CountdownText from "./CountdownText";
 
 // ! console.log() s ar e intentionally left here for backend implementation
 function Countdown() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [isPlaying, setIsPlaying] = useBoolean(true);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [isPlaying, setIsPlaying] = useBoolean(true);
 
-  // Cleanup function for leaving the queue (may be extended for specific scenarios: timeout, matched)
-  const leaveQueueHandler = () => {
-    // TODO API call to leave queue, may require some information from the redux store
-    console.log("Call API to leave the queue");
-    setIsPlaying.off();
-    dispatch(leaveQueue());
-  };
+	// Cleanup function for leaving the queue (may be extended for specific scenarios: timeout, matched)
+	const leaveQueueHandler = () => {
+		// TODO API call to leave queue, may require some information from the redux store
+		console.log("Call API to leave the queue");
+		setIsPlaying.off();
+		dispatch(leaveQueue());
+	};
 
-  const completeTimeHandler = () => {
-    console.log(`Times up. Leaving queue.`);
-    leaveQueueHandler();
-  };
+	const completeTimeHandler = () => {
+		console.log(`Times up. Leaving queue.`);
+		leaveQueueHandler();
+	};
 
-  const updateTimeHandler = () => {
-    console.log(`API update call made`);
-    // Random number generator to simulate matching
-    const r = Math.random() * 100;
-    if (r > 85) {
-      console.log("Found buddy");
-      leaveQueueHandler();
-      // TODO Integration with the backend
-      navigate("/session/df22c77e-312b-4edf-8b52-b78425572506");
-    }
-  };
+	const updateTimeHandler = (remainingTime: number) => {
+		// Prevents t=0 redirect.
+		if (remainingTime === 30) {
+			return;
+		}
 
-  return (
-    <Stack spacing={6} align="center">
-      <Text fontWeight={600} fontSize="2xl">
-        Finding a buddy....
-      </Text>
-      <CountdownCircleTimer
-        isPlaying={isPlaying}
-        duration={30}
-        colors="#004777"
-        onUpdate={updateTimeHandler}
-        onComplete={completeTimeHandler}
-      >
-        {({ remainingTime }) => <CountdownText remainingTime={remainingTime} />}
-      </CountdownCircleTimer>
-      <Button colorScheme="red" onClick={leaveQueueHandler}>
-        Leave Queue
-      </Button>
-    </Stack>
-  );
+		console.log(`API update call made`);
+		// Random number generator to simulate matching
+		const r = Math.random() * 100;
+		if (r > 85) {
+			console.log("Found buddy");
+			leaveQueueHandler();
+			// TODO Integration with the backend
+			navigate("/session/df22c77e-312b-4edf-8b52-b78425572506");
+		}
+	};
+
+	return (
+		<Stack spacing={6} align="center">
+			<Text fontWeight={600} fontSize="2xl">
+				Finding a buddy....
+			</Text>
+			<CountdownCircleTimer
+				isPlaying={isPlaying}
+				duration={30}
+				colors="#004777"
+				onUpdate={updateTimeHandler}
+				onComplete={completeTimeHandler}
+			>
+				{({ remainingTime }) => <CountdownText remainingTime={remainingTime} />}
+			</CountdownCircleTimer>
+			<Button colorScheme="red" onClick={leaveQueueHandler}>
+				Leave Queue
+			</Button>
+		</Stack>
+	);
 }
 
 export default Countdown;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,37 +8,30 @@ import Countdown from "../components/matching/Countdown";
 import TempQueueForm from "../components/form/TempQueueForm";
 
 function Home() {
-  const inQueue = useSelector((state: RootState) => state.matching.inQueue);
+	const inQueue = useSelector((state: RootState) => state.matching.inQueue);
 
-  return (
-    <>
-      <Navbar />
-      {/* Expected Responsive problems to happen here */}
-      <Grid
-        templateColumns="2fr 1fr"
-        gap={10}
-        minW="65vw"
-        maxW="80vw"
-        mx="auto"
-        my={16}
-      >
-        <Box>
-          <Heading>Attempt History</Heading>
-          <Box borderRadius="md" boxShadow="lg" p={8}>
-            This space will be used for user history or a list of problems
-          </Box>
-        </Box>
+	return (
+		<>
+			<Navbar />
+			{/* Expected Responsive problems to happen here */}
+			<Grid templateColumns="2fr 1fr" gap={10} minW="65vw" maxW="80vw" mx="auto" my={16}>
+				<Box>
+					<Heading>Attempt History</Heading>
+					<Box borderRadius="md" boxShadow="lg" p={8}>
+						This space will be used for user history or a list of problems
+					</Box>
+				</Box>
 
-        <Box>
-          <Heading>Start Coding!</Heading>
-          <Box borderRadius="md" boxShadow="lg" p={8}>
-            {/* {inQueue ? <Countdown /> : <QueueForm />} */}
-            {inQueue ? <Countdown /> : <TempQueueForm />}
-          </Box>
-        </Box>
-      </Grid>
-    </>
-  );
+				<Box>
+					<Heading>Start Coding!</Heading>
+					<Box borderRadius="md" boxShadow="lg" p={8}>
+						{/* {inQueue ? <Countdown /> : <QueueForm />} */}
+						{inQueue ? <Countdown /> : <TempQueueForm />}
+					</Box>
+				</Box>
+			</Grid>
+		</>
+	);
 }
 
 export default Home;
