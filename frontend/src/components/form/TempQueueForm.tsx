9d771c7150ea5799a9db09a68a2344ba3450bd38
KwanHW <kwanhaowei@gmail.com>
2022-09-12 19:20:33 +0800

    feat: integrate with backend

--- frontend/src/components/form/TempQueueForm.tsx
+++ frontend/src/components/form/TempQueueForm.tsx
@@ -1,21 +1,25 @@
-import { Button, Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import { Button, Radio, RadioGroup, Stack, useToast } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import axios from "../../axios";
 import { enterQueue } from "../../feature/matching/matchingSlice";
+import { JoinQueueResponse, JoinQueueRequest } from "../../proto/matching-service";
+import { QuestionDifficulty } from "../../proto/types";
 
 const DIFFICULTY = [
-  {
-    name: "Easy",
-    colorScheme: "green",
-  },
-  {
-    name: "Medium",
-    colorScheme: "orange",
-  },
-  {
-    name: "Hard",
-    colorScheme: "red",
-  },
+	{
+		name: "Easy",
+		colorScheme: "green",
+	},
+	{
+		name: "Medium",
+		colorScheme: "orange",
+	},
+	{
+		name: "Hard",
+		colorScheme: "red",
+	},
 ];
 
 /**
@@ -23,34 +27,59 @@ const DIFFICULTY = [
  *  Once we are able to support multiple difficulties, this component will be deprecated.
  */
 function TempQueueForm() {
-  const dispatch = useDispatch();
-  const [selectedDiff, setSelectedDiff] = useState("Easy");
+	const toast = useToast();
+	const sessionToken = useSelector((state: RootState) => state.user.sessionToken);
+	const dispatch = useDispatch();
+	const [selectedDiff, setSelectedDiff] = useState("Easy");
 
-  const enterQueueHandler = () => {
-    // API call to enter queue, probably may need to pass some information to redux store
+	const enterQueueHandler = () => {
+		// API call to enter queue
+		const difficulty: QuestionDifficulty = DIFFICULTY.findIndex((d) => d.name === selectedDiff) + 1;
+		const joinQueueReq: JoinQueueRequest = { sessionToken, difficulty };
 
-    // For now just change the flag
-    dispatch(enterQueue());
-  };
+		// Not necessary since backend will check also (and should NOT trigger this at all)
+		// Will probably show something meaningful
+		if (difficulty === 0) {
+			console.error("No difficulty selected");
+		}
+		axios
+			.post<JoinQueueResponse>("/queue/join", joinQueueReq)
+			.then((res) => {
+				const { errorCode, errorMessage } = res.data;
 
-  return (
-    <RadioGroup onChange={setSelectedDiff} value={selectedDiff}>
-      <Stack spacing={6}>
-        {DIFFICULTY.map((d) => (
-          <Radio value={d.name} key={d.name}>
-            <Button
-              onClick={() => setSelectedDiff(d.name)}
-              colorScheme={d.colorScheme}
-              variant="solid"
-            >
-              {`${d.name} Difficulty`}
-            </Button>
-          </Radio>
-        ))}
-        <Button onClick={enterQueueHandler}>Find Buddy</Button>
-      </Stack>
-    </RadioGroup>
-  );
+				if (errorCode) {
+					throw new Error(errorMessage);
+				}
+
+				// For now just change the flag
+				dispatch(enterQueue());
+			})
+			.catch((err) => {
+				toast({
+					title: "Error",
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+					position: "top",
+					description: err.message,
+				});
+			});
+	};
+
+	return (
+		<RadioGroup onChange={setSelectedDiff} value={selectedDiff}>
+			<Stack spacing={6}>
+				{DIFFICULTY.map((d) => (
+					<Radio value={d.name} key={d.name}>
+						<Button onClick={() => setSelectedDiff(d.name)} colorScheme={d.colorScheme} variant="solid">
+							{`${d.name} Difficulty`}
+						</Button>
+					</Radio>
+				))}
+				<Button onClick={enterQueueHandler}>Find Buddy</Button>
+			</Stack>
+		</RadioGroup>
+	);
 }
 
 export default TempQueueForm;
--- frontend/src/components/matching/Countdown.tsx
+++ frontend/src/components/matching/Countdown.tsx
@@ -1,66 +1,98 @@
-import { Button, Stack, Text, useBoolean } from "@chakra-ui/react";
-import { useDispatch } from "react-redux";
+import { Button, Stack, Text, useBoolean, useToast } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useNavigate } from "react-router-dom";
+import axios from "../../axios";
 import { leaveQueue } from "../../feature/matching/matchingSlice";
 import CountdownText from "./CountdownText";
+import { CheckQueueStatusRequest, CheckQueueStatusResponse, QueueStatus } from "../../proto/matching-service";
+import { RootState } from "../../app/store";
 
 // ! console.log() s ar e intentionally left here for backend implementation
 function Countdown() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [isPlaying, setIsPlaying] = useBoolean(true);
+	const sessionToken = useSelector((state: RootState) => state.user.sessionToken);
+	const toast = useToast();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [isPlaying, setIsPlaying] = useBoolean(true);
 
-  // Cleanup function for leaving the queue (may be extended for specific scenarios: timeout, matched)
-  const leaveQueueHandler = () => {
-    // TODO API call to leave queue, may require some information from the redux store
-    console.log("Call API to leave the queue");
-    setIsPlaying.off();
-    dispatch(leaveQueue());
-  };
+	// Cleanup function for leaving the queue (may be extended for specific scenarios: timeout, matched)
+	const leaveQueueHandler = () => {
+		// TODO API call to leave queue, may require some information from the redux store
+		console.log("Call API to leave the queue");
+		setIsPlaying.off();
+		dispatch(leaveQueue());
+	};
 
-  const completeTimeHandler = () => {
-    console.log(`Times up. Leaving queue.`);
-    leaveQueueHandler();
-  };
+	const completeTimeHandler = () => {
+		console.log(`Times up. Leaving queue.`);
+		leaveQueueHandler();
+	};
 
-  const updateTimeHandler = (remainingTime: number) => {
-    // Prevents t=0 redirect.
-    if (remainingTime === 30) {
-      return;
-    }
+	const updateTimeHandler = (remainingTime: number) => {
+		// Prevents t=0 redirect.
+		if (remainingTime === 30) {
+			return;
+		}
 
-    console.log(`API update call made`);
-    // Random number generator to simulate matching
-    const r = Math.random() * 100;
-    if (r > 85) {
-      console.log("Found buddy");
-      leaveQueueHandler();
-      // TODO Integration with the backend
-      navigate("/session/df22c77e-312b-4edf-8b52-b78425572506");
-    }
-  };
+		console.log(`API update call made`);
+		const checkRequest: CheckQueueStatusRequest = { sessionToken };
+		axios
+			.post<CheckQueueStatusResponse>("/queue/status", checkRequest)
+			.then((res) => {
+				const { errorCode, errorMessage, queueStatus, roomToken } = res.data;
 
-  return (
-    <Stack spacing={6} align="center">
-      <Text fontWeight={600} fontSize="2xl">
-        Finding a buddy....
-      </Text>
-      <CountdownCircleTimer
-        isPlaying={isPlaying}
-        duration={30}
-        colors="#004777"
-        onUpdate={updateTimeHandler}
-        onComplete={completeTimeHandler}
-      >
-        {({ remainingTime }) => <CountdownText remainingTime={remainingTime} />}
-      </CountdownCircleTimer>
-      <Button colorScheme="red" onClick={leaveQueueHandler}>
-        Leave Queue
-      </Button>
-    </Stack>
-  );
+				if (errorCode) {
+					throw new Error(errorMessage);
+				}
+
+				switch (queueStatus) {
+					case QueueStatus.PENDING:
+						break;
+					case QueueStatus.MATCHED:
+						// Leave the queue and transit to session
+						leaveQueueHandler();
+						navigate(`/session/${roomToken}`);
+						break;
+					case QueueStatus.EXPIRED:
+						completeTimeHandler();
+						break;
+					default:
+						throw new Error("Invalid queueStatus");
+				}
+			})
+			.catch((err) => {
+				toast({
+					title: "Error",
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+					position: "top",
+					description: err.message,
+				});
+			});
+	};
+
+	return (
+		<Stack spacing={6} align="center">
+			<Text fontWeight={600} fontSize="2xl">
+				Finding a buddy....
+			</Text>
+			<CountdownCircleTimer
+				isPlaying={isPlaying}
+				duration={30}
+				colors="#004777"
+				onUpdate={updateTimeHandler}
+				onComplete={completeTimeHandler}
+			>
+				{({ remainingTime }) => <CountdownText remainingTime={remainingTime} />}
+			</CountdownCircleTimer>
+			<Button colorScheme="red" onClick={leaveQueueHandler}>
+				Leave Queue
+			</Button>
+		</Stack>
+	);
 }
 
 export default Countdown;
