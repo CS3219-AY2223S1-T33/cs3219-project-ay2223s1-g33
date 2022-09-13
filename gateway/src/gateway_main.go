4e3b01786869c6e9ccf7023589e4049b7a63f3dc
sharpstorm <chimchar17@gmail.com>
2022-09-13 23:12:17 +0800

    feat: add websocket proxying to gateway

---
+++ gateway/src/gateway_main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	gw "cs3219-project-ay2223s1-g33/gateway/gateway"
+	"log"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+func registerGatewayRoutes(ctx context.Context, config *GatewayConfiguration) (http.Handler, error) {
+	mux := runtime.NewServeMux(runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.HTTPBodyMarshaler{
+		Marshaler: &runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				Multiline:       true,
+				UseEnumNumbers:  true,
+				EmitUnpopulated: true,
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true,
+			},
+		},
+	}))
+
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	log.Printf("Proxying to User-BFF on %s\n", config.UserBFFServer)
+	err := gw.RegisterUserBFFServiceHandlerFromEndpoint(ctx, mux, config.UserBFFServer, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Proxying to Matching on %s\n", config.MatchingServer)
+	err = gw.RegisterQueueServiceHandlerFromEndpoint(ctx, mux, config.MatchingServer, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return mux, nil
+}
