37f6880fe66be790b2b1b536c78ff55662f629ec
sharpstorm <chimchar17@gmail.com>
2022-09-21 01:48:51 +0800

    chore: add versioning to gateway

--- gateway/src/env_loader.go
+++ gateway/src/env_loader.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 )
 
 type GatewayConfiguration struct {
@@ -10,6 +11,8 @@ type GatewayConfiguration struct {
 	CollabServer   string
 	SessionServer  string
 	StaticServer   string
+
+	Port int
 }
 
 const (
@@ -18,6 +21,7 @@ const (
 	envCollabServer   = "COLLAB_SERVER"
 	envSessionServer  = "SESSION_SERVER"
 	envStaticServer   = "STATIC_SERVER"
+	envPort           = "GATEWAY_PORT"
 )
 
 func loadConfig() *GatewayConfiguration {
@@ -26,6 +30,7 @@ func loadConfig() *GatewayConfiguration {
 	collabServer := loadEnvVariableOrDefaultString(envCollabServer, "localhost:4002")
 	sessionServer := loadEnvVariableOrDefaultString(envSessionServer, "localhost:4100")
 	staticServer := loadEnvVariableOrDefaultString(envStaticServer, "localhost:8000")
+	port := loadEnvVariableOrDefaultInt(envPort, 5000)
 
 	return &GatewayConfiguration{
 		UserBFFServer:  userBFFServer,
@@ -33,6 +38,7 @@ func loadConfig() *GatewayConfiguration {
 		CollabServer:   collabServer,
 		SessionServer:  sessionServer,
 		StaticServer:   staticServer,
+		Port:           port,
 	}
 }
 
@@ -42,4 +48,18 @@ func loadEnvVariableOrDefaultString(envKey string, defaultValue string) string {
 		return defaultValue
 	}
 	return value
+}
+
+func loadEnvVariableOrDefaultInt(envKey string, defaultValue int) int {
+	value, found := os.LookupEnv(envKey)
+	if !found {
+		return defaultValue
+	}
+
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+
+	return intValue
 }
