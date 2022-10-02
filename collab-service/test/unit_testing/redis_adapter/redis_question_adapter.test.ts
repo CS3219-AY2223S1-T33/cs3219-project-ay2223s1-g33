b6e79fa19b490417b317e521ad9cab51bacb37f7
ooawagaeri <oastreach@gmail.com>
2022-10-02 11:52:32 +0800

    chore: change mock redis package

--- collab-service/test/unit_testing/redis_adapter/redis_question_adapter.test.ts
+++ collab-service/test/unit_testing/redis_adapter/redis_question_adapter.test.ts
@@ -1,11 +1,11 @@
 import setQuestionRedis from '../../../src/redis_adapter/redis_question_adapter';
 import { Question } from '../../../src/proto/types';
 
-const redisMock = require('redis-mock');
+const Redis = require('ioredis-mock');
 
 describe('Function-Redis-Question setQuestionRedis', () => {
   test('Test question is saved on redis', async () => {
-    const redis = redisMock.createClient();
+    const redis = new Redis();
     const question: Question = {
       questionId: 1,
       name: '1',
