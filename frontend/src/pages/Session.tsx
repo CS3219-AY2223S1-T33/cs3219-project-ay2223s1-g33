1cfcc28172ff88f49e8d19cf10e591725293e146
kwanhw <kwanhaowei@gmail.com>
2022-10-07 00:11:02 +0800

    fix: remove client-side initiation for question

--- frontend/src/pages/Session.tsx
+++ frontend/src/pages/Session.tsx
@@ -33,12 +33,12 @@ function Session() {
   const {
     isOpen: isLeaveModalOpen,
     onOpen: onOpenLeaveModal,
-    onClose: onCloseLeaveModal,
+    onClose: onCloseLeaveModal
   } = useDisclosure();
   const {
     isOpen: isDisconnectModalOpen,
     onOpen: onOpenDisconnectModal,
-    onClose: onCloseDisconnectModal,
+    onClose: onCloseDisconnectModal
   } = useDisclosure();
   const toast = useFixedToast();
 
@@ -92,14 +92,13 @@ function Session() {
 
       ws.on("user_join", (joinedNickname: Nickname) => {
         toast.sendSuccessMessage("", {
-          title: `${joinedNickname.nickname} has joined the room!`,
+          title: `${joinedNickname.nickname} has joined the room!`
         });
-        ws.sendQuestionRequest();
       });
 
       ws.on("user_leave", (leftNickname: Nickname) => {
         toast.sendAlertMessage("", {
-          title: `${leftNickname.nickname} has left the room.`,
+          title: `${leftNickname.nickname} has left the room.`
         });
       });
 
@@ -110,7 +109,7 @@ function Session() {
 
       ws.on("question_get", (q: { question: string }) => {
         const questionObj: Question = Question.fromJsonString(q.question);
-        toast.sendInfoMessage("Retrieved question");
+        toast.sendInfoMessage("Question loaded");
         setQuestion(questionObj);
       });
 
@@ -121,7 +120,7 @@ function Session() {
       // Yjs initialisation
       const tempyDoc = new Y.Doc();
       const params: { [x: string]: string } = {
-        room: roomToken === undefined ? "" : roomToken,
+        room: roomToken === undefined ? "" : roomToken
       };
 
       const tempprovider = buildWSProvider(tempyDoc, params);
