cb55eb7b44aed703d8e09bfb7f8b22c7eb8cec88
ooawagaeri <oastreach@gmail.com>
2022-10-07 01:12:11 +0800

    fix: change create alert name

--- collab-service/src/message_handler/room/connect_message_builder.ts
+++ collab-service/src/message_handler/room/connect_message_builder.ts
@@ -8,7 +8,7 @@ export const OPCODE_QUESTION_RCV = 9;
 export const OPCODE_SAVE_CODE_SEND = 10;
 export const OPCODE_SAVE_CODE_ACK = 11;
 
-function createConnectionAlertPackage(content: string, opcode: number): Uint8Array {
+function encodeContentOpcode(content: string, opcode: number): Uint8Array {
   const encoder = encoding.createEncoder();
   encoding.writeUint8(encoder, opcode);
   encoding.writeVarString(encoder, content);
@@ -16,15 +16,15 @@ function createConnectionAlertPackage(content: string, opcode: number): Uint8Arr
 }
 
 function createConnectedPackage(username: string): Uint8Array {
-  return createConnectionAlertPackage(username, OPCODE_USER_JOIN);
+  return encodeContentOpcode(username, OPCODE_USER_JOIN);
 }
 
 function createDisconnectedPackage(username: string): Uint8Array {
-  return createConnectionAlertPackage(username, OPCODE_USER_LEAVE);
+  return encodeContentOpcode(username, OPCODE_USER_LEAVE);
 }
 
 function createQuestionRcvPackage(question: string): Uint8Array {
-  return createConnectionAlertPackage(question, OPCODE_QUESTION_RCV);
+  return encodeContentOpcode(question, OPCODE_QUESTION_RCV);
 }
 
 /**
