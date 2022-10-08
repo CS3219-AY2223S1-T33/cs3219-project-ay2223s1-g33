9c51d2f8c195b650e5ffebc3d9d02a7ca1eb6e57
sharpstorm <chimchar17@gmail.com>
2022-10-08 19:41:16 +0800

    refactor: update history service db adapter

--- history-service/src/index.ts
+++ history-service/src/index.ts
@@ -9,27 +9,40 @@ import LoopbackApiChannel from './api_server/loopback_channel';
 import { IHistoryCrudService } from './proto/history-crud-service.grpc-server';
 import Constants from './constants';
 import Logger from './utils/logger';
+import { connectDatabase } from './db';
 
-const version = `${Constants.VERSION_MAJOR}.${Constants.VERSION_MINOR}.${Constants.VERSION_REVISION}`;
-Logger.info(`Starting History Service [V${version}]`);
+function printVersion() {
+  const version = `${Constants.VERSION_MAJOR}.${Constants.VERSION_MINOR}.${Constants.VERSION_REVISION}`;
+  Logger.info(`Starting History Service [V${version}]`);
+}
 
-const envConfig = loadEnvironment();
+async function run() {
+  printVersion();
 
-const dataStore: AppStorage = new AppStorage();
+  const envConfig = loadEnvironment();
+  const dbConnection = await connectDatabase(envConfig);
+  const dataStore: AppStorage = new AppStorage(dbConnection);
 
-const apiServer = getApiServer(envConfig.HTTP_PORT, envConfig.GRPC_PORT);
-const expressApp = apiServer.getHttpServer();
+  const apiServer = getApiServer(envConfig.HTTP_PORT, envConfig.GRPC_PORT);
+  const expressApp = apiServer.getHttpServer();
 
-expressApp.get('/', (_: Request, resp: Response) => {
-  resp.status(200).send('Welcome to History Service');
-});
+  expressApp.get('/', (_: Request, resp: Response) => {
+    resp.status(200).send('Welcome to History Service');
+  });
 
-const crudController = new HistoryCrudServiceApi(dataStore, envConfig.USER_SERVICE_URL);
-apiServer.registerServiceRoutes(crudController);
+  const crudController = new HistoryCrudServiceApi(
+    dataStore,
+    envConfig.USER_SERVICE_URL,
+    envConfig.QUESTION_SERVICE_URL,
+  );
+  apiServer.registerServiceRoutes(crudController);
 
-const loopbackCrudController = new LoopbackApiChannel<IHistoryCrudService>();
-loopbackCrudController.registerServiceRoutes(crudController);
-const historyServiceController = new HistoryServiceApi(loopbackCrudController);
-apiServer.registerServiceRoutes(historyServiceController);
+  const loopbackCrudController = new LoopbackApiChannel<IHistoryCrudService>();
+  loopbackCrudController.registerServiceRoutes(crudController);
+  const historyServiceController = new HistoryServiceApi(loopbackCrudController);
+  apiServer.registerServiceRoutes(historyServiceController);
 
-apiServer.bind();
+  apiServer.bind();
+}
+
+run();
