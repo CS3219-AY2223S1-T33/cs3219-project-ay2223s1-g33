409d316f923d9c5292a5b17db929e12d1b5a95a8
kwanhw <kwanhaowei@gmail.com>
2022-10-09 22:59:30 +0800

    chore: bump y-webscoket

--- scripts/gen-y-websocket.sh
+++ scripts/gen-y-websocket.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cd y-websocket
+VER=`npm pkg get version | sed 's/\"//g'`
+PKG_NAME="y-websocket-peerprep-${VER}.tgz"
 echo "Generating package"
 npm pack --pack-destination ../frontend
 
 cd ../frontend
 echo "Installing package"
-npm i --force y-websocket-peerprep-1.4.5.tgz
+npm i --force $PKG_NAME
 
 echo "Done"
