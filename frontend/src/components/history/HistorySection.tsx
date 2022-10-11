9bb226898e1f1e19634ec9069a4a5132c403f05e
sharpstorm <chimchar17@gmail.com>
2022-10-11 23:59:05 +0800

    feat: add frontend for submission loading

--- frontend/src/components/history/HistorySection.tsx
+++ frontend/src/components/history/HistorySection.tsx
@@ -1,18 +1,35 @@
-import { Code, Divider, Heading, VStack } from "@chakra-ui/react";
+import { Code, Divider, Heading, VStack, Spinner, Center } from "@chakra-ui/react";
 import React from "react";
 
 type Props = {
-  submission: string;
+  submission: string | undefined;
 };
 
 function HistorySection({ submission }: Props) {
+  let contentSection = (
+    <Center width="100%">
+      <VStack spacing={4} width="100%">
+        <Spinner size='xl' />
+        <br/>
+        <Heading as="h5" size="sm">
+          Loading Submission...
+        </Heading>
+      </VStack>
+    </Center>
+  );
+
+  if (submission) {
+    contentSection = (
+      <Code width="100%" overflow="scroll">{submission}</Code>
+    );
+  }
   return (
     <VStack spacing={4} width="50%">
       <Heading as="h4" size="md">
         Code Submitted
       </Heading>
       <Divider />
-      <Code overflow="scroll">{submission}</Code>
+      {contentSection}
     </VStack>
   );
 }
