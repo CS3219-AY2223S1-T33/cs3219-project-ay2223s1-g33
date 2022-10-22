ec3090202d62bc778bef5bb846bed8d778ea0e28
kwanhw <kwanhaowei@gmail.com>
2022-10-22 20:55:43 +0800

    chore: add validators

--- frontend/src/constants/validators.ts
+++ frontend/src/constants/validators.ts
@@ -1,14 +1,23 @@
 import * as yup from "yup";
 
-const LOGIN_VALIDATOR = yup.object().shape({
+const AUTH_SCHEMA = {
 	email: yup.string().required("Please enter your email."),
 	password: yup.string().required("Please enter your password."),
-});
+	confirmPassword: yup
+		.string()
+		.required("Please enter your confirmed password.")
+		.oneOf([yup.ref("password"), "Please ensure your passwords match."]),
+};
+
+const LOGIN_VALIDATOR = yup.object().shape(AUTH_SCHEMA);
 
 const REGISTER_VALIDATOR = yup.object().shape({
+	...AUTH_SCHEMA,
 	nickname: yup.string().required("Please enter your nickname."),
-	email: yup.string().required("Please enter your email."),
-	password: yup.string().required("Please enter your password."),
 });
 
-export { LOGIN_VALIDATOR, REGISTER_VALIDATOR };
+const RESET_PW_VALIDATIOR = yup.object().shape({
+	email: AUTH_SCHEMA.email,
+});
+
+export { LOGIN_VALIDATOR, REGISTER_VALIDATOR, RESET_PW_VALIDATIOR };
