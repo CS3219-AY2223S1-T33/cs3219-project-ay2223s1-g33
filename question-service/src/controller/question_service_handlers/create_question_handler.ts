e5863b9e9c74f241e2051969c1a46ea9716f03c4
Zheng Teck <adamgoh1998@gmail.com>
2022-10-30 12:15:31 +0800

    chore: update question return value

--- question-service/src/controller/question_service_handlers/create_question_handler.ts
+++ question-service/src/controller/question_service_handlers/create_question_handler.ts
@@ -1,86 +1,70 @@
-import { Question, QuestionDifficulty } from "../../proto/types"
-import {
-	CreateQuestionRequest,
-	CreateQuestionResponse,
-} from "../../proto/question-service"
-import {
-	ApiRequest,
-	ApiResponse,
-	IApiHandler,
-} from "../../api_server/api_server_types"
-import { IStorage, IQuestionStore } from "../../storage/storage.d"
-import { StoredQuestion } from "../../model/question_store_model"
+import { Question, QuestionDifficulty } from '../../proto/types';
+import { CreateQuestionRequest, CreateQuestionResponse } from '../../proto/question-service';
+import { ApiRequest, ApiResponse, IApiHandler } from '../../api_server/api_server_types';
+import { IStorage, IQuestionStore } from '../../storage/storage.d';
+import { StoredQuestion } from '../../model/question_store_model';
 
-class CreateQuestionHandler
-	implements IApiHandler<CreateQuestionRequest, CreateQuestionResponse>
-{
-	questionStore: IQuestionStore
+class CreateQuestionHandler implements IApiHandler<CreateQuestionRequest, CreateQuestionResponse> {
+  questionStore: IQuestionStore;
 
-	constructor(storage: IStorage) {
-		this.questionStore = storage.getQuestionStore()
-	}
+  constructor(storage: IStorage) {
+    this.questionStore = storage.getQuestionStore();
+  }
 
-	async handle(
-		apiRequest: ApiRequest<CreateQuestionRequest>
-	): Promise<ApiResponse<CreateQuestionResponse>> {
-		const { request } = apiRequest
+  async handle(
+    apiRequest: ApiRequest<CreateQuestionRequest>,
+  ): Promise<ApiResponse<CreateQuestionResponse>> {
+    const { request } = apiRequest;
 
-		if (!request.question) {
-			return CreateQuestionHandler.buildErrorResponse(
-				"Invalid question information"
-			)
-		}
-
-		if (
-			request.question.content.length === 0 ||
-			request.question.name.length === 0 ||
-			request.question.solution.length === 0 ||
-			request.question.executionInput.length === 0
-		) {
-			return CreateQuestionHandler.buildErrorResponse(
-				"Missing question information"
-			)
-		}
-
-    if (request.question.difficulty === QuestionDifficulty.UNUSED || 
-      !Object.values(QuestionDifficulty).includes(request.question.difficulty)) {
-        return CreateQuestionHandler.buildErrorResponse(
-          "Invalid question difficulty"
-        )
+    if (!request.question) {
+      return CreateQuestionHandler.buildErrorResponse('Invalid question information');
     }
 
-		let question: StoredQuestion | undefined
-		try {
-			question = await this.questionStore.addQuestion(request.question)
-		} catch (err) {
-			return CreateQuestionHandler.buildErrorResponse(`${err}`)
-		}
+    if (
+      request.question.content.length === 0
+      || request.question.name.length === 0
+      || request.question.solution.length === 0
+      || request.question.executionInput.length === 0
+    ) {
+      return CreateQuestionHandler.buildErrorResponse('Missing question information');
+    }
 
-		const resultQuestionModel: Question = {
-			questionId: question.questionId!,
-			...question,
-		}
+    if (
+      request.question.difficulty === QuestionDifficulty.UNUSED
+      || !Object.values(QuestionDifficulty).includes(request.question.difficulty)
+    ) {
+      return CreateQuestionHandler.buildErrorResponse('Invalid question difficulty');
+    }
 
-		return {
-			response: {
-				question: resultQuestionModel,
-				errorMessage: "",
-			},
-			headers: {},
-		}
-	}
+    let question: StoredQuestion | undefined;
+    try {
+      question = await this.questionStore.addQuestion(request.question);
+    } catch (err) {
+      return CreateQuestionHandler.buildErrorResponse(`${err}`);
+    }
 
-	static buildErrorResponse(
-		errorMessage: string
-	): ApiResponse<CreateQuestionResponse> {
-		return {
-			response: {
-				question: undefined,
-				errorMessage,
-			},
-			headers: {},
-		}
-	}
+    const resultQuestionModel: Question = {
+      ...question,
+    };
+
+    return {
+      response: {
+        question: resultQuestionModel,
+        errorMessage: '',
+      },
+      headers: {},
+    };
+  }
+
+  static buildErrorResponse(errorMessage: string): ApiResponse<CreateQuestionResponse> {
+    return {
+      response: {
+        question: undefined,
+        errorMessage,
+      },
+      headers: {},
+    };
+  }
 }
 
-export default CreateQuestionHandler
+export default CreateQuestionHandler;
