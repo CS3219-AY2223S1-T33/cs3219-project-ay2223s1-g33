f12ec8dea287ba7ed0b79cbbdb707c698110b5fc
kwanhw <kwanhaowei@gmail.com>
2022-11-07 21:07:44 +0800

    fix: add media queries for table on home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Heading } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../components/ui/navbar/HomeNavbar";
@@ -7,6 +7,9 @@ import Countdown from "../components/matching/Countdown";
 import QueueForm from "../components/form/QueueForm";
 import HistoryTable from "../components/history/HistoryTable";
 
+const SMALL_AREA = `"queue" "question"`;
+const DEFAULT_AREA = `"question queue"`;
+
 function Home() {
   const inQueue = useSelector((state: RootState) => state.matching.inQueue);
   const hiddenColumns: string[] = ["attemptId", "users"];
@@ -14,26 +17,27 @@ function Home() {
     <>
       <Navbar />
       <Grid
-        templateColumns="70% 30%"
+        templateAreas={{ base: SMALL_AREA, lg: DEFAULT_AREA }}
+        templateRows={{ lg: "100%" }}
+        templateColumns={{ base: "1fr", lg: "75% 25%" }}
         gap={10}
-        minW="70vw"
-        maxW="85vw"
+        w={{ base: "95vw", lg: "90vw" }}
         mx="auto"
         my={16}
       >
-        <Box>
+        <GridItem area="question">
           <Heading>Attempt History</Heading>
           <Box borderRadius="md" boxShadow="lg" p={8}>
             <HistoryTable hiddenColumns={hiddenColumns} />
           </Box>
-        </Box>
+        </GridItem>
 
-        <Box>
+        <GridItem area="queue">
           <Heading>Start Coding!</Heading>
           <Box borderRadius="md" boxShadow="lg" p={8}>
             {inQueue ? <Countdown /> : <QueueForm />}
           </Box>
-        </Box>
+        </GridItem>
       </Grid>
     </>
   );
